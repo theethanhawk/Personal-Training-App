ce0928bbce6781ef0bc08b763a60a3a3aa738578
Ethan Montclair <ethmontclair@gmail.com>
2024-03-05 11:32:12 -0700

    program weeks has bug

--- pt_programs_app/urls.py
+++ pt_programs_app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     # Metrics dashboard
     path('', views.programsHome, name='programs'),
     path('create_program/', views.createProgram, name='create_program'),
+    path('program/<int:pk>/', views.program_details, name='program_details'),
+    path('build_program_weeks/', views.build_program_weeks, name='build_program_weeks'),
+    #path('display_full_program/', views.display_program, name="display_program"),
 ]
--- pt_programs_app/views.py
+++ pt_programs_app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 
-from .forms import TrainingProgramForm
+from .forms import TrainingProgramForm, TrainingWeekForm
 
 from .models import TrainingProgram, ActivityType
 
@@ -31,9 +31,51 @@ def createProgram(request):
                     activity_type, created = ActivityType.objects.get_or_create(title=a)
                     program.activity.add(activity_type)
 
-            return redirect('pt_programs_app:programs')
+            return redirect('pt_programs_app:program_details', pk=program.pk)
     
     context = {
         'form': form,
     }
     return render(request, 'pt_programs_app/create_program.html', context)
+
+
+@login_required
+def program_details(request, pk):
+    """Renders program form data with button to continue building program"""
+    program = get_object_or_404(TrainingProgram, pk=pk)
+    return render(request, 'pt_programs_app/program_details.html', {'program': program})
+
+
+@login_required
+def build_program_weeks(request, pk):
+    """Renders TrainingWeek forms for # of weeks in program_duration"""
+    program = get_object_or_404(TrainingProgram, pk=pk)
+    weeks = program.program_duration
+    
+    if request.method != 'POST':
+        # Create a list of TrainingWeekForm instances based on the number of weeks in the program.
+        forms = [TrainingWeekForm(prefix=str(i)) for i in range(weeks)]
+
+        context = {
+            'program': program,
+            'forms': forms,
+        }
+
+        return render(request, 'pt_programs_app/build_program_weeks.html', context)
+
+    else:
+        # Create a list to store form instances with cleaned data.
+        form_instances = []
+
+        # Loop through each week's form in the request data.
+        for i in range(weeks):
+            form = TrainingWeekForm(request.POST, prefix=str(i))
+            
+            if form.is_valid():
+                # If the form is valid, save it to the list of form instances.
+                week_instance = form.save(commit=False)
+                week_instance.program = program  # Associate the week with the correct program
+                week_instance.save()
+                form_instances.append(week_instance)
+
+        return redirect('pt_programs_app:program_details', pk=program.pk)
